db4f3bc7221207d7706e3d9ec9a9b46529002001
Kevin Zúñiga <kevin.zun@gmail.com>
2019-12-29 16:37:05 -0500

    2019 - Day 4

---
+++ 2019/4b-secure-container.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func check(a int) bool {
+	number := []rune(strconv.Itoa(a))
+
+	var last rune = '0'
+	digits := make(map[rune]int)
+
+	for _, c := range number {
+		if c < last {
+			return false
+		}
+
+		digits[c] += 1
+		last = c
+	}
+
+	for _, count := range digits {
+		if count == 2 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	var n, m int
+
+	fmt.Scanf("%d-%d", &n, &m)
+
+	s := 0
+	for i := n; i <= m; i++ {
+		if check(i) {
+			s++
+		}
+	}
+
+	fmt.Println(s)
+}
