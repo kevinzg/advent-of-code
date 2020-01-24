7b03ff0d7b1f811369abff157c8f0dfa9d071c12
Kevin Zúñiga <kevin.zun@gmail.com>
2020-01-23 23:27:40 -0500

    2015 - Day 4

---
+++ 2015/04a-advent-coins.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'digest'
+
+key = gets.strip
+
+i = 0
+
+while true
+  md5 = Digest::MD5.hexdigest(key + i.to_s)
+  if md5.start_with? '00000'
+    puts i
+    break
+  end
+  i += 1
+end
