6d5704e02dfc769c2238913c3dab1a8507b87529
Kevin Zúñiga <kevin.zun@gmail.com>
2021-12-05 16:40:31 -0500

    2021 - Day 5

---
+++ 2021/05b-hydrothermal-venture.lua
@@ -0,0 +1,64 @@
+local lines = {}
+
+for line in io.lines() do
+   local next = line:gmatch '%d+'
+   local p = {
+      x = 1 + next(),
+      y = 1 + next(),
+   }
+   local q = {
+      x = 1 + next(),
+      y = 1 + next(),
+   }
+   table.insert(lines, {
+      p = p,
+      q = q,
+   })
+end
+
+local function dir(a, b)
+   if a > b then
+      return -1
+   elseif b > a then
+      return 1
+   else
+      return 0
+   end
+end
+
+local map = {}
+
+local mark_map = function(x, y)
+   local key = x .. ',' .. y
+   if map[key] == nil then
+      map[key] = 1
+   else
+      map[key] = map[key] + 1
+   end
+end
+
+for _, line in ipairs(lines) do
+   local dx = dir(line.p.x, line.q.x)
+   local dy = dir(line.p.y, line.q.y)
+
+   local x = line.p.x
+   local y = line.p.y
+
+   while x ~= line.q.x or y ~= line.q.y do
+      mark_map(x, y)
+      x = x + dx
+      y = y + dy
+   end
+
+   mark_map(x, y)
+end
+
+local count = 0
+
+for _, v in pairs(map) do
+   if v > 1 then
+      count = count + 1
+   end
+end
+
+print(count)
