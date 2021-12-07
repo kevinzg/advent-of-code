3f17b1bcd0f750b7152091b365306a17366ba9c2
Kevin Zúñiga <kevin.zun@gmail.com>
2021-12-06 21:37:34 -0500

    2021 - Day 6

---
+++ 2021/06b-lanternfish.lua
@@ -0,0 +1,28 @@
+local input = io.read '*a'
+local state = {}
+
+for d in input:gmatch '%d+' do
+   d = tonumber(d)
+   state[d] = (state[d] or 0) + 1
+end
+
+for _ = 1, 256 do
+   local next_gen = {}
+   for t, count in pairs(state) do
+      if t == 0 then
+         next_gen[6] = (next_gen[6] or 0) + count
+         next_gen[8] = count
+      else
+         next_gen[t - 1] = (next_gen[t - 1] or 0) + count
+      end
+   end
+   state = next_gen
+end
+
+local total = 0
+
+for _, count in pairs(state) do
+   total = total + count
+end
+
+print(total)
